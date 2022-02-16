e3fed76432488763d660d3e24822093533e5fbad
Iustin <iustin6893@gmail.com>
2022-02-16 20:54:38 +0100

    Valido prueba1

--- prueba1/src/prueba1/prueba1.java
+++ prueba1/src/prueba1/prueba1.java
@@ -1,11 +1,11 @@
 package prueba1;
 
 public class prueba1 {
-
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo java-git");
-
+		System.out.println("Pruebas Git. Clase Cambio1");
 	}
 
 }
